fe777f783ea99c05c350a1417c1abb80f56ad8e8
jae-ng <s10242508@connect.np.edu.sg>
2022-12-04 16:56:28 +0800

    formatting updates

--- js-files/reserve.js
+++ js-files/reserve.js
@@ -18,28 +18,4 @@ function handleLogin(e){
     alert("Reservation Success!")
     return output;
   }
-
-const swiper = new Swiper('.swiper', {
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-      el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-      el: '.swiper-scrollbar',
-  },
-
-  autoplay: {
-      delay: 3000,
-  },
-});
   
